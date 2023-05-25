eb0f35cfb86154ecb3f566c7f67bb87a69e45737
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-05-25 15:34:22 +0700

    constructor, getter and setter for RentalRequest model

--- src/main/java/com/example/bookrentalsystem/controllers/RentalRequest.java
+++ src/main/java/com/example/bookrentalsystem/controllers/RentalRequest.java
@@ -1,4 +1,65 @@
 package com.example.bookrentalsystem.controllers;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
 public class RentalRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User borrower;
+
+    @ManyToOne
+    private Book book;
+
+    private boolean accepted;
+
+    // Constructor
+    public RentalRequest() {
+
+    }
+
+    public RentalRequest(User borrower, Book book, boolean accepted) {
+        this.borrower = borrower;
+        this.book = book;
+        this.accepted = accepted;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(User borrower) {
+        this.borrower = borrower;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public boolean isAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        this.accepted = accepted;
+    }
+
 }
