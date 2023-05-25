933e6d3b4048dc12349e4487903b8f6ddcb6e897
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-05-25 15:38:19 +0700

    constructor getter and setter for Book model

--- src/main/java/com/example/bookrentalsystem/controllers/Book.java
+++ src/main/java/com/example/bookrentalsystem/controllers/Book.java
@@ -13,5 +13,47 @@ public class Book {
     private String author;
     private int stock;
 
-    // constructors, getters, setters
+    // Constructor
+    public Book(){
+
+    }
+
+    public Book(String title, String author, int stock) {
+        this.title = title;
+        this.author = author;
+        this.stock = stock;
+    }
+
+    // Getter and Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public void setAuthor() {
+        this.author = author;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock){
+        this.stock = stock;
+    }
 }
